402c883ea1c6ce0bcddc8a8be0c1d98e3f3ccbf0
Naziur-Rahman <naziurr85@gmail.com>
2024-07-30 01:40:43 +0600

    change Hearo section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,17 @@
 
-import Hero from "../components/Hero/Hero";
-
+import videoBg from "../assets/video/water.mp4";
 const Home = () => {
     return (
-        <>
-           <Hero></Hero> 
-        </>
+        <div>
+            <div>
+            <div className="relative h-screen">
+                <video autoPlay loop muted 
+                className="absolute right-0 h-screen w-full object-cover z-[-1] bottom-0">
+                    <source src={videoBg} type="video/mp4"/>
+                </video>
+            </div>
+        </div>
+        </div>
     );
 };
 
